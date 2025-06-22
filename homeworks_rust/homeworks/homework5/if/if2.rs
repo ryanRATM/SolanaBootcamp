e5d7514044fe7f34c24bee5db7072d2b981f47c5
Lucas Ryan <lucasryan121@gmail.com>
2025-06-22 15:52:53 +0000

    rustlings hw 5&6

--- homeworks_rust/homeworks/homework5/if/if2.rs
+++ homeworks_rust/homeworks/homework5/if/if2.rs
@@ -4,14 +4,16 @@
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 // Execute the command `rustlings hint if2` if you want a hint :)
 
-// I AM NOT DONE
 
 pub fn fizz_if_foo(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
     } 
+    else if fizzish == "fuzz" {
+        "bar"
+    }
     else {
-        1
+        "baz"
     }
 }
 
@@ -19,7 +21,7 @@ pub fn fizz_if_foo(fizzish: &str) -> &str {
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
     fn foo_for_fizz() {
         assert_eq!(fizz_if_foo("fizz"), "foo")
